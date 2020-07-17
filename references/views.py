17a76744ce5909ea2c217a31911281b2370fbd59
Jair Aguilar <apjair97@gmail.com>
2020-07-16 22:50:31 -0500

    [Modified] Dir tree was channged to improve the file sorting and menu nav was starting

--- references/views.py
+++ references/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def references_db(request):
-    return render(request, 'app_refcodes.html')
+    return render(request, 'references/reference.html')
