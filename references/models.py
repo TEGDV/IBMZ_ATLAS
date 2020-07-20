3a97ebabc958dec938cd76fc1a5797425a8f7a77
Jair Aguilar <apjair97@gmail.com>
2020-07-20 08:01:06 -0500

    added images files folder and files folder to static files folder

--- references/models.py
+++ references/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Refcode(models.Model):
     # DB references codes table model
 
-    ref_code = models.TextField(unique=True, max_length=35)
-    system_number = models.TextField(max_length=30,blank=True)
-    operation_number = models.TextField(max_length=30)
-    operation_name = models.TextField(max_length=30)
+    ref_code = models.CharField(unique=True, max_length=35)
+    system_number = models.CharField(max_length=30,blank=True)
+    operation_number = models.CharField(max_length=30)
+    operation_name = models.CharField(max_length=30)
     hmdescription = models.TextField(max_length=300, blank=True)
     fix_action = models.TextField(max_length=300,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    created_by = models.TextField(max_length=30)
+    created_by = models.CharField(max_length=30)
--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from users.models import EmployeeProfile
 # Register your models here, if you want to see them into the Django admin site insted of use 
 # Your database console.
-admin.site.register(EmployeeProfile)
+@admin.register(EmployeeProfile)
+class EmployeeProfileAdmin(admin.ModelAdmin):
+    #Profile admin
+    pass
