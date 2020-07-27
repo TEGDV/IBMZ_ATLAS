fe4f4e919bb778a10e4fae6b8fc38e790ddb94b2
YirDev <apjair97@gmail.com>
2020-07-27 04:03:19 -0500

    added config to deploy on heroku

--- code_ibmzatlas/settings.py
+++ code_ibmzatlas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0p1%73udm*3^#ngfoaybdhromvk#!j_4#0!c5iy-51v#n&nkdg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -132,3 +133,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join (BASE_DIR, "static"), '/ Var/www/static /',]
 
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
